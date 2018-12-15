4f1ca115448521c4524013a9a4dd4bf9aada9377
Nick Yang <nick.yang3721@gmail.com>
2018-12-15 16:10:45 -0500

    Add basic click command

--- ranker.py
+++ ranker.py
@@ -1,6 +1,15 @@
 from flask import Flask
+import click
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.cli.command()
+def start():
+    value = click.prompt('Please enter a valid integer', type=int)
+    print('your value is')
+    print(value)
